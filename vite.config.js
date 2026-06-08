d737abc84bb58cce7d93d3240a1163b4e99a0f2f
hsong <hsong705@126.com>
2026-06-08 17:00:34 +0800

    chore: 配置 GitHub Pages 部署

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/guizhou-tourism-dashboard/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
